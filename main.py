753eec1522bcd637dc93083f0bbb7f624d43f52b
aadityacooldude9 <88792172+aadityacooldude9@users.noreply.github.com>
2024-09-29 23:04:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 10)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+import random
+import turtle as turtle_module
+tim = turtle_module.Turtle()
+turtle_module.colormode(255)
+
+color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (39, 216, 69), (238, 227, 5), (227, 159, 49)]
+
+tim.up()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 101
+tim.speed("fastest")
+tim.hideturtle()
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
